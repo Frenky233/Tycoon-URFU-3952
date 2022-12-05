fefefce7e6411cebe071238709fe5c454271f6ed
DuckStormGG <Ghost56YT@gmail.com>
2022-12-05 16:32:58 +0500

    upDate Main Menu

--- Tycoon/Assets/UI/mainMenu/Buttons.cs
+++ Tycoon/Assets/UI/mainMenu/Buttons.cs
@@ -6,10 +6,18 @@ using UnityEngine.UIElements;
 public class Buttons : MonoBehaviour
 {
     public AudioSource sound;
+    public Object Object;
     public Button newGameButton;
     public Button loadGameButton;
     public Button settingsButton;
     public Button exitButton;
+    public VisualElement menu;
+    public VisualElement MainMenu;
+    public Button menuExit;
+    public float uiBaseScreenHeight = 500f;
+
+
+
     // Start is called before the first frame update
     void Start()
     {
@@ -19,25 +27,40 @@ public class Buttons : MonoBehaviour
         loadGameButton = root.Q<Button>("buttonLoadGame");
         settingsButton = root.Q<Button>("buttonSettings");
         exitButton = root.Q<Button>("ButtonExit");
-
+        MainMenu = root.Q<VisualElement>("Container");
+        menu = root.Q<VisualElement>("Menu");
+        menuExit = root.Q<Button>("menuExit");
+        menu.style.display = DisplayStyle.None;
+        
         newGameButton.clicked += NewGameButtonClick;
         loadGameButton.clicked += LoadGameButtonClick;
         settingsButton.clicked += SettingsButtonClick;
         exitButton.clicked += ExitButtonClick;
-
-
+        menuExit.clicked +=menuExitEvent;
+    }
+    private void Update() 
+    {
+        newGameButton.style.fontSize = GetScaledFontSize(80);
+        loadGameButton.style.fontSize = GetScaledFontSize(80);
+        settingsButton.style.fontSize = GetScaledFontSize(80);
+        exitButton.style.fontSize = GetScaledFontSize(80);
+        menuExit.style.fontSize = GetScaledFontSize(50);
     }
     void NewGameButtonClick()
     {
         ClickSoud();
+        
     }
     void LoadGameButtonClick()
     {
         ClickSoud();
+        menuOpenEvemt();
     }
     void SettingsButtonClick()
     {
+        menuOpenEvemt();
         ClickSoud();
+
     }
     void ExitButtonClick()
     {
@@ -47,6 +70,27 @@ public class Buttons : MonoBehaviour
     void ClickSoud()
     {
         sound.Play();
+    }
+    void menuOpenEvemt()
+    {
+        menu.style.display = DisplayStyle.Flex;
+        MainMenu.style.display = DisplayStyle.None;
+        menu.style.opacity = 1;
+        MainMenu.style.opacity = 0;
+    }
+    void menuExitEvent()
+    {
+        ClickSoud();
+        menu.style.display = DisplayStyle.None;
+        MainMenu.style.display = DisplayStyle.Flex;
+        menu.style.opacity = 0;
+        MainMenu.style.opacity = 1;
+    }
+    private int GetScaledFontSize (int baseFontSize) 
+    {
+        float uiScale = Screen.height / uiBaseScreenHeight;
+        int scaledFontSize = Mathf.RoundToInt(baseFontSize * uiScale);
+        return scaledFontSize;
     }
 
 }
